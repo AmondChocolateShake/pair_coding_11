9cf01170854b3a604fec8fa9c5bb306fd4d78d32
AmondChocolateShake <dongju230130@gmail.com>
2023-02-28 17:10:08 +0900

    new setting

---
+++ style.js
@@ -0,0 +1,56 @@
+body = {
+  margin : "0px",
+  w: "100vh",
+  h: "100vh",
+}
+
+
+//root
+root = {
+  display: "flex",
+  flex_direction: "column",
+  align_items: "center"
+}
+//c1
+firstContainer = {
+  w: "210px",
+  h: "35px",
+}
+
+//c2
+secondContainer = {
+  w: "295px",
+  h: "186px"
+}
+
+
+//제목
+head = {
+  font_size: "24px",
+  margin: "0px",
+  text_align: "center"
+}
+
+//제목 아래 바
+bar= {
+  color: "black",
+  w: "199px",
+  h: "2px",
+  margin: "0px"
+}
+
+
+context={
+
+  
+}
+
+//원
+circle = {
+  w: "82px",
+  h: "82px",
+  border_radius_left: "50px", 
+  border_radius_right : "50px",
+  background_color: "#5a5a5a",
+
+}
