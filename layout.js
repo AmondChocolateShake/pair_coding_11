63b730d44e0a29cf4d8ab5b9d58b5db2e4ae84bd
AmondChocolateShake <dongju230130@gmail.com>
2023-02-28 16:17:39 +0900

    Revert "3:00"
    This reverts commit ca36bc618079088bbe2be8482dd6c3ebb2c34e2a.

--- layout.js
+++ layout.js
@@ -1,2 +1,31 @@
-import { trainingData } from "./TData";
+
+
+
+
+
+
+let root = document.getElementById("root");
+
+//div 태그를 만드는 함수
+function createDiv(){
+  let div=document.createElement("div");
+  return div;
+}
+
+
+//div태그를 원하는 수 만큼 생성해서 root의 자식요소로 지정
+function createDivChildren(root,num){
+  for(let i=0;i<num;i+=1){
+    root.appendChild(createDiv());
+  }
+  return root.children
+}
+
+
+let outline=createDivChildren(root,2);
+
+
+
+
+
 
