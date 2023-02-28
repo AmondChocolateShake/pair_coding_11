b2d31eb995f33425abeaf2b986ab40b1741c3075
AmondChocolateShake <dongju230130@gmail.com>
2023-02-28 17:36:29 +0900

    rechange function

--- layout.js
+++ layout.js
@@ -13,32 +13,28 @@ function createDiv(){
 }
 
 
-//div태그를 원하는 수 만큼 생성해서 root의 자식요소로 지정
-function createDivChildren(root,num){
+//div태그를 원하는 수 만큼 생성해서 parent의 자식요소로 지정
+function createDivChildren(parent,num){
   for(let i=0;i<num;i+=1){
-    root.appendChild(createDiv());
+    parent.appendChild(createDiv());
   }
-  return root.children;
+  return parent.children;
 }
 
 
-//가장 큰 컨테이너
+//가장 큰 컨테이너 2개 생성
 let outContainers= createDivChildren(root,2);
 
 
 //container1의 자식 요소 추가
-let Header=createDiv();
-let Line=createDiv();
-let Ctxt=createDiv();
-outContainers[0].appendChild(Header);
-outContainers[0].appendChild(Line);
-outContainers[0].appendChild(Ctxt);
+createDivChildren(outContainers[0],3);
 
 
 //container2의 자식요소 추가
-let Circle=createDiv();
-outContainers[1].appendChild(Circle);
+createDivChildren(outContainers[1],1);
 
+
+//-------스타일------
 
 
 
--- style.js
+++ style.js
@@ -1,4 +1,6 @@
-body = {
+
+
+let body = {
   margin : "0px",
   w: "100vh",
   h: "100vh",
@@ -6,33 +8,33 @@ body = {
 
 
 //root
-root = {
+let root = {
   display: "flex",
   flex_direction: "column",
   align_items: "center"
 }
 //c1
-firstContainer = {
+let firstContainer = {
   w: "210px",
   h: "35px",
 }
 
 //c2
-secondContainer = {
+let secondContainer = {
   w: "295px",
   h: "186px"
 }
 
 
 //제목
-head = {
+let head = {
   font_size: "24px",
   margin: "0px",
   text_align: "center"
 }
 
 //제목 아래 바
-bar= {
+let bar= {
   color: "black",
   w: "199px",
   h: "2px",
@@ -40,13 +42,12 @@ bar= {
 }
 
 
-context={
-
+let context={
   
 }
 
 //원
-circle = {
+let circle = {
   w: "82px",
   h: "82px",
   border_radius_left: "50px", 
